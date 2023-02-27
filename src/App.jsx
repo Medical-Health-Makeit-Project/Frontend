b7092c0f31c5b4fab9ed8d4ec6319d2a8b369305
JeanVittory <vittory.dev@gmail.com>
2023-02-27 12:09:27 -0500

    feat(components, app.jsx): fixing media queries in footer
    Fixing the media queries at footer putting a best position between components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Layout></Layout>
+      <Layout>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+      </Layout>
     </div>
   );
 }
