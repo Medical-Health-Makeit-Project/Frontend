c16bd169156709d43db5286c390e0e3b41a7a875
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-03-01 10:47:04 -0500

    feat(departments.pages.jsx): create a departments component

--- src/pages/home/Home.pages.jsx
+++ src/pages/home/Home.pages.jsx
@@ -1,10 +1,12 @@
 import { Welcome } from './components/welcome';
+import { Departments } from './components/our-departments/departments.pages';
 import './home.pages.css';
 
 export const Home = () => {
   return (
     <main>
       <Welcome />
+      <Departments />
     </main>
   );
 };
