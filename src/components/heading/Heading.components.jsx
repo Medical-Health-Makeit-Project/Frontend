c050cd5c0f5b15ad599c2a65feb7038afee36948
JeanVittory <vittory.dev@gmail.com>
2023-03-05 22:07:39 -0500

    feat(component heading): building the heading component to each page
    Building the heading component to each page with its respective breadcrump

---
+++ src/components/heading/Heading.components.jsx
@@ -0,0 +1,24 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+import image from '@assets/heading.jpeg';
+import './heading.components.scss';
+
+export const Heading = ({ title }) => {
+  let pathname = new URL(window.location).pathname.split('/').splice(1);
+  return (
+    <header className="header-container">
+      <img src={image} alt="background-image" className="header__image" />
+      <div className="header-title-breadcrumb">
+        <h1 className="header__title">{title}</h1>
+        <Breadcrumb className="breadcrump">
+          {pathname.map((element) => {
+            return (
+              <BreadcrumbItem key={element}>
+                <BreadcrumbLink href="#">{element}</BreadcrumbLink>
+              </BreadcrumbItem>
+            );
+          })}
+        </Breadcrumb>
+      </div>
+    </header>
+  );
+};
