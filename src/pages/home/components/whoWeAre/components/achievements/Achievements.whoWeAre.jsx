00e5d631cc9c9960f2c5486887895e9294c391b0
JeanVittory <vittory.dev@gmail.com>
2023-03-06 14:30:40 -0500

    feat(achievements): creating a service to achievements compoenent in home page

--- src/pages/home/components/whoWeAre/components/achievements/Achievements.whoWeAre.jsx
+++ src/pages/home/components/whoWeAre/components/achievements/Achievements.whoWeAre.jsx
@@ -1,54 +1,49 @@
 import { TfiMedallAlt, TfiThumbUp } from 'react-icons/tfi';
 import { TbBed } from 'react-icons/tb';
 import { AiOutlineExperiment } from 'react-icons/ai';
-import { v4 as uuidv4 } from 'uuid';
+import { Skeleton } from '@chakra-ui/react';
 import { Icon } from '@components/icon';
 import './achievements.whoWeAre.scss';
 
-export const Achievements = () => {
-  const achievements = [
-    {
-      id: uuidv4(),
-      icon: <TfiMedallAlt color="white" size="24" />,
-      quantity: '20+',
-      achievement: 'Years of Experience',
-    },
-    {
-      id: uuidv4(),
-      icon: <TfiThumbUp color="white" size="24" />,
-      quantity: '99%',
-      achievement: 'Patients Satisfied',
-    },
-    {
-      id: uuidv4(),
-      icon: <TbBed color="white" size="24" />,
-      quantity: '700+',
-      achievement: 'Medical Beds',
-    },
-    {
-      id: uuidv4(),
-      icon: <AiOutlineExperiment color="white" size="24" />,
-      quantity: '50+',
-      achievement: 'Laboratory Experts',
-    },
-  ];
+export const Achievements = ({ achievements }) => {
+  const icons = {
+    TfiMedallAlt: <TfiMedallAlt color="white" size={24} />,
+    TfiThumbUp: <TfiThumbUp color="white" size={24} />,
+    TbBed: <TbBed color="white" size={24} />,
+    AiOutlineExperiment: <AiOutlineExperiment color="white" size={24} />,
+  };
 
   return (
     <article className="whoWeAre-achievements">
       <ul className="whoWeAre-achievements__ul">
-        {achievements.map((element) => {
-          return (
-            <li key={element.id} className="whoWeAre-achievements__li">
-              <Icon color="info" size="md">
-                {element.icon}
-              </Icon>
-              <div className="whoWeAre-achievements-info">
-                <p className="whoWeAre-achievements-info__quantity">{element.quantity}</p>
-                <p className="whoWeAre-achievements-info__achievement">{element.achievement}</p>
-              </div>
-            </li>
-          );
-        })}
+        {!achievements.length
+          ? Object.keys(icons).map((element) => {
+              return (
+                <Skeleton className="whoWeAre-achievements__li" height={'230px'}>
+                  <li>
+                    <Icon color="info" size="md"></Icon>
+                    <div className="whoWeAre-achievements-info">
+                      <p className="whoWeAre-achievements-info__quantity"></p>
+                      <p className="whoWeAre-achievements-info__achievement"></p>
+                    </div>
+                  </li>
+                </Skeleton>
+              );
+            })
+          : achievements.map((element) => {
+              let icon = icons[element.icon];
+              return (
+                <li key={element.id} className="whoWeAre-achievements__li">
+                  <Icon color="info" size="md">
+                    {icon}
+                  </Icon>
+                  <div className="whoWeAre-achievements-info">
+                    <p className="whoWeAre-achievements-info__quantity">{element.quantity}</p>
+                    <p className="whoWeAre-achievements-info__achievement">{element.achievement}</p>
+                  </div>
+                </li>
+              );
+            })}
       </ul>
     </article>
   );
