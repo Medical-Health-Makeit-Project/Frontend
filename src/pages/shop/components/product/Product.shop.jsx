c88e4a9257b368aadf5157f50686244766022d94
JeanVittory <vittory.dev@gmail.com>
2023-03-10 11:23:19 -0500

    feat(shop page): building the shop UI with services and filters
    Building the shop UI with services and filters

--- src/pages/shop/components/product/Product.shop.jsx
+++ src/pages/shop/components/product/Product.shop.jsx
@@ -1,7 +1,21 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useWindowSize } from '@hooks';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { PropTypes } from 'prop-types';
 import { ProductImage } from '../productImage';
 import { ProductInfo } from '../productInfo';
 import { ProductQuantity } from '../productQuantity';
 import { ProductBuyButton } from '../productBuyButton';
+import { ContentModal } from '../modal';
 import './product.shop.scss';
 
 export const Product = ({
@@ -15,17 +29,76 @@ export const Product = ({
   price,
   stock,
 }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const windowSize = useWindowSize();
   return (
-    <article className="product">
-      <ProductImage
-        id={id}
-        image={image}
-        discount={discount}
-        product={product}
-      />
-      <ProductInfo product={product} dose={dose} price={price} label={label} />
-      <ProductQuantity stock={stock} />
-      <ProductBuyButton />
-    </article>
+    <>
+      <article className="product">
+        <ProductImage
+          id={id}
+          image={image}
+          discount={discount}
+          product={product}
+          setShowModal={onOpen}
+        />
+        <ProductInfo
+          product={product}
+          dose={dose}
+          price={price}
+          label={label}
+        />
+        <ProductQuantity stock={stock} />
+        <ProductBuyButton />
+      </article>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        onEsc
+        size={
+          windowSize > 900
+            ? '3xl'
+            : windowSize > 700
+            ? '2xl'
+            : windowSize > 400
+            ? 'md'
+            : 'xs'
+        }
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader />
+          <ModalCloseButton />
+          <ModalBody>
+            <ContentModal
+              description={description}
+              dose={dose}
+              image={image}
+              label={label}
+              price={price}
+              product={product}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <ProductBuyButton />
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
+};
+
+Product.propTypes = {
+  id: PropTypes.string.isRequired,
+  product: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  dose: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  discount: PropTypes.number,
+  price: PropTypes.number.isRequired,
+  stock: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  discount: 0,
 };
--- src/pages/shop/components/productImage/ProductImage.shop.jsx
+++ src/pages/shop/components/productImage/ProductImage.shop.jsx
@@ -1,10 +1,42 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { PropTypes } from 'prop-types';
 import './productImage.shop.scss';
 
-export const ProductImage = ({ id, image, discount, product }) => {
+export const ProductImage = ({
+  id,
+  image,
+  discount,
+  product,
+  setShowModal,
+}) => {
+  const handlerOpenModal = () => {
+    setShowModal(true);
+  };
   return (
     <section className="image-container">
       {!discount || <div className="discount-flag">{discount}%</div>}
-      <img src={image} alt={product} className="image-container__image" height={'280px'} width="280px" />
+      <img
+        src={image}
+        alt={product}
+        className="image-container__image"
+        height="280px"
+        width="280px"
+        onClick={handlerOpenModal}
+        onKeyDown={handlerOpenModal}
+        role="presentation"
+      />
     </section>
   );
+};
+
+ProductImage.propTypes = {
+  id: PropTypes.string.isRequired,
+  product: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  discount: PropTypes.number,
+  setShowModal: PropTypes.func.isRequired,
+};
+
+ProductImage.defaultProps = {
+  discount: 0,
 };
