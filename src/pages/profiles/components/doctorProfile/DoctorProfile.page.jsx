9b8ae09bbfb841cbd681b89c0475ca2c6c67d6e8
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-04-09 23:21:55 -0500

    chore(userprofile.page.jsx doctorprofile.page.jsx): added functionality to update some data

--- src/pages/profiles/components/doctorProfile/DoctorProfile.page.jsx
+++ src/pages/profiles/components/doctorProfile/DoctorProfile.page.jsx
@@ -1,6 +1,8 @@
-import { BiMap } from 'react-icons/bi';
+import { useState } from 'react';
+import { BiMap, BiEdit } from 'react-icons/bi';
 import { BsTelephoneOutbound, BsGenderAmbiguous, BsCheckLg } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
+import { Button } from '@components/buttons';
 
 import './doctorProfile.page.scss';
 
@@ -16,11 +18,44 @@ export const DoctorProfile = ({
   qualifications,
   skills,
 }) => {
+  const editStyleOn = 'editOn';
+  const editStyleOff = 'editOff';
+
+  const dataStyleOn = 'dataOn';
+  const dataStyleOff = 'dataOff';
+
+  const [editState, setEditState] = useState(editStyleOff);
+  const [prevData, setPrevData] = useState(dataStyleOn);
+  const [emailStatus, setEmailStauts] = useState(email);
+  const [phoneStatus, setPhoneSatus] = useState(phone);
+
+  const handleEditOn = () => {
+    setEditState(editStyleOn);
+    setPrevData(dataStyleOff);
+  };
+
+  const handleEditOff = () => {
+    setEditState(editStyleOff);
+    setPrevData(dataStyleOn);
+  };
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmailStauts(newEmail);
+  };
+  const handlePhoneChange = (event) => {
+    const newPhone = event.target.value;
+    setPhoneSatus(newPhone);
+  };
+
   return (
     <>
       <main className="doctor__profile-main">
         <section className="doctor__card">
           <article className="primary__info-container">
+            <div className="edit__container-doctor">
+              <BiEdit size={20} className="icons" onClick={handleEditOn} />
+            </div>
             <div className="avatar__main-container ">
               <picture className="profile__img-container">
                 <img src={avatar} alt="Profile image" />
@@ -63,20 +98,48 @@ export const DoctorProfile = ({
 
             <div className="email__info">
               <h3>Email</h3>
-              <div className="info__containers">
+              <div className={editState}>
+                <input
+                  type="text"
+                  className="edit__input"
+                  placeholder={emailStatus}
+                  required
+                  onChange={handleEmailChange}
+                  pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}"
+                />
+              </div>
+              <div className={`info__containers ${prevData}`}>
                 <AiOutlineMail size={18} className="icons" />
-                <p>{email}</p>
+                <p>{emailStatus}</p>
               </div>
             </div>
 
             <div className="phone__info">
               <h3>Phone</h3>
-              <div className="info__containers">
+              <div className={editState}>
+                <input
+                  type="text"
+                  className="edit__input"
+                  placeholder={phoneStatus}
+                  required
+                  onChange={handlePhoneChange}
+                  pattern="/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/"
+                />
+              </div>
+              <div className={`info__containers ${prevData}`}>
                 <BsTelephoneOutbound size={18} className="icons" />
-                <p>{phone}</p>
+                <p>{phoneStatus}</p>
               </div>
             </div>
           </article>
+          <Button
+            variant="solid"
+            color="info"
+            className={`${editState} save__button`}
+            onClick={handleEditOff}
+          >
+            Save changes
+          </Button>
         </section>
       </main>
     </>
