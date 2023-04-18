b50370a8c24f2a7c685e05f466cba979e64a4a06
JeanVittory <vittory.dev@gmail.com>
2023-04-18 01:23:26 -0500

    feat(administration): the Update and UI doctors were builded

--- src/pages/administration/components/doctors/Doctors.administration.jsx
+++ src/pages/administration/components/doctors/Doctors.administration.jsx
@@ -1,12 +1,15 @@
 import { Form } from './components/form';
 import { DoctorsList } from './components/doctorsList';
+import { DoctorContext } from './context/doctors.context';
 import './doctors.administration.scss';
 
 export const Doctors = () => {
   return (
-    <main className="doctors-admin__main">
-      <Form />
-      <DoctorsList />
-    </main>
+    <DoctorContext>
+      <main className="doctors-admin__main">
+        <Form />
+        <DoctorsList />
+      </main>
+    </DoctorContext>
   );
 };
--- src/pages/administration/components/doctors/components/doctorsList/DoctorsList.doctors.administration.jsx
+++ src/pages/administration/components/doctors/components/doctorsList/DoctorsList.doctors.administration.jsx
@@ -1,8 +1,54 @@
-import './doctorsList.doctors.administration.scss';
 import { allDoctorsSWR } from '@services/allDoctors';
+import { Button } from '@components/buttons';
+import { useDoctorContext } from '../../context/doctors.context';
+import { v4 as uuid } from 'uuid';
+import './doctorsList.doctors.administration.scss';
 
 export const DoctorsList = () => {
   const { allDoctors, allDoctorsError, allDoctorsIsLoading } = allDoctorsSWR();
 
-  return <div>Hello!</div>;
+  const { setDoctorToBeUpdated } = useDoctorContext();
+
+  const handleSetFormToUpdate = (doctor) => {
+    setDoctorToBeUpdated({
+      firstname: doctor.firstname,
+      lastname: doctor.lastname,
+      email: doctor.email,
+      birthdate: doctor.birthdate,
+      area: doctor.area,
+      avatar: doctor.avatar,
+      phone: doctor.phone,
+      location: { city: doctor.headquarter.city, country: doctor.headquarter.country },
+      gender: doctor.gender,
+      qualifications: [...doctor.qualifications],
+      memberships: [...doctor.memberships],
+      skills: [...doctor.skills],
+    });
+  };
+
+  return (
+    <section className="doctor-list">
+      {allDoctors?.map((doctor) => {
+        return (
+          <article key={uuid()} className="doctor-list__item">
+            <div className="doctor-list__avatar-container">
+              <img src={doctor.avatar} alt="Avatar" className="avatar" />
+            </div>
+            <section className="doctor-list-info">
+              <h3 className="doctor-list-info__fullname">{`${doctor.firstname} ${doctor.lastname}`}</h3>
+              <p className="doctor-list-area">{doctor.area}</p>
+            </section>
+            <section className="buttons-action-container">
+              <Button variant="outline" color="danger">
+                Delete
+              </Button>
+              <Button color="info" onClick={() => handleSetFormToUpdate(doctor)}>
+                Update
+              </Button>
+            </section>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
