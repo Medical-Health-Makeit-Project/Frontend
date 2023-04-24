50e108398e6db39777017de1e7955d3f256bfcff
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-04-24 12:19:05 -0500

    chore(register.page.jsx): console.log deleted

--- src/pages/register/Register.page.jsx
+++ src/pages/register/Register.page.jsx
@@ -76,6 +76,7 @@ export const Register = () => {
     repeatPassword,
     termsAndConditions,
   } = userData;
+
   return (
     <section className="register__container">
       <Heading title="Register" image={headingImage} />
