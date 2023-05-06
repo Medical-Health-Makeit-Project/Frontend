f1d869b16e5b15b803a7516a5d9c2f0b97e2861e
JeanVittory <vittory.dev@gmail.com>
2023-05-06 15:00:16 -0500

    perf(migrating .env variables): migrating .env variables

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,93 +4,78 @@ export const IMAGE_MAX_SIZE = 5242880;
 export const DOCTOR_EMAIL_DOMAIN = '@drmebid.com';
 export const DOCTOR_PREFIX = 'Dr';
 export const DATE_FORMAT = 'DD/MM/YYYY';
-export const STRIPE_PUBLIC_KEY =
-  'pk_test_51Mz5GfGuKX0jSjOwt0aR0OYVxnjYpKnCPWm4LeMrNgJQz3pDNVnnX3C2faSkG1uhwtsoYKfb7B3aPtkMASQqEMiR00s5c12Uux';
+export const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
 
 // PRODUCTS
 
 //CHECK
-export const ALL_PRODUCTS = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/products' : null;
+export const ALL_PRODUCTS = !import.meta.env.PROD ? import.meta.env.VITE_ALL_PRODUCTS : null;
 
 // CHECK
 export const PRODUCTS_BY_CATEGORY = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/products/'
+  ? import.meta.env.VITE_PRODUCTS_BY_CATEGORY
   : null;
 
 // CHECK
-export const CATEGORIES = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/categories' : null;
+export const CATEGORIES = !import.meta.env.PROD ? import.meta.env.VITE_CATEGORIES : null;
 
 // CHECK
-export const POST_PRODUCTS = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/products' : null;
+export const POST_PRODUCTS = !import.meta.env.PROD ? import.meta.env.VITE_POST_PRODUCTS : null;
 
 // CHECK
-export const DELETE_PRODUCT = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/products'
-  : null;
+export const DELETE_PRODUCT = !import.meta.env.PROD ? import.meta.env.VITE_DELETE_PRODUCT : null;
 
 //CHECK
-export const UPDATE_PRODUCTS = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/products'
-  : null;
+export const UPDATE_PRODUCTS = !import.meta.env.PROD ? import.meta.env.VITE_UPDATE_PRODUCTS : null;
 
 //-----------------------------------------------------------
 
 // AUTHENTICATION
 
 //check
-export const AUTHENTICATION = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/auth/' : null;
+export const AUTHENTICATION = !import.meta.env.PROD ? import.meta.env.VITE_AUTHENTICATION : null;
 
 //check
-export const AUTHORIZATION = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/auth/authorization'
-  : null;
+export const AUTHORIZATION = !import.meta.env.PROD ? import.meta.env.VITE_AUTHORIZATION : null;
 
 //-----------------------------------------------------------
 
 // DOCTORS
 
 //check
-export const POST_DOCTOR = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/auth/register/doctor'
-  : null;
+export const POST_DOCTOR = !import.meta.env.PROD ? import.meta.env.VITE_POST_DOCTOR : null;
 
 //check
-export const UPDATE_DOCTOR = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/doctors' : null;
+export const UPDATE_DOCTOR = !import.meta.env.PROD ? import.meta.env.VITE_UPDATE_DOCTOR : null;
 
-export const DELETE_DOCTOR = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/doctors' : null;
-
-//check
-export const ALL_DOCTORS = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/doctors' : null;
-
-export const DOCTORS_BY_AREA = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/doctors/areas'
-  : null;
+export const DELETE_DOCTOR = !import.meta.env.PROD ? import.meta.env.VITE_DELETE_DOCTOR : null;
 
 //check
-export const LOCATIONS = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/locations' : null;
+export const ALL_DOCTORS = !import.meta.env.PROD ? import.meta.env.VITE_ALL_DOCTORS : null;
+
+export const DOCTORS_BY_AREA = !import.meta.env.PROD ? import.meta.env.VITE_DOCTORS_BY_AREA : null;
+
+//check
+export const LOCATIONS = !import.meta.env.PROD ? import.meta.env.VITE_LOCATIONS : null;
 
 //-----------------------------------------------------------
 
 // USERS
 
 //check
-export const REGISTER_USER = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/auth/register'
-  : null;
+export const REGISTER_USER = !import.meta.env.PROD ? import.meta.env.VITE_REGISTER_USER : null;
 
 // check
-export const UPDATE_USER = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/users' : null;
+export const UPDATE_USER = !import.meta.env.PROD ? import.meta.env.VITE_UPDATE_USER : null;
 
 //-----------------------------------------------------------
 
 // PAYMENTS
 
-export const PAYMENTS = !import.meta.env.PROD ? 'http://localhost:5150/api/v1/payments' : null;
+export const PAYMENTS = !import.meta.env.PROD ? import.meta.env.VITE_PAYMENTS : null;
 
 //-----------------------------------------------------------
 
 // APPOINTMENTS
 
-export const APPOINTMENTS = !import.meta.env.PROD
-  ? 'http://localhost:5150/api/v1/appointments'
-  : null;
+export const APPOINTMENTS = !import.meta.env.PROD ? import.meta.env.VITE_APPOINTMENTS : null;
